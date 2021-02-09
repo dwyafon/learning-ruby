f4b3ae90a6550dc9f427612589acb2eaa7665682
Shaw Malcolm <shawmalcolm@protonmail.com>
2021-02-09 13:11:40 +0000

    update and rename chapter 2 file

---
+++ chapter-2-numbers.rb
@@ -0,0 +1,21 @@
+puts 1 + 2
+puts 1.0 + 2.0
+puts 2.0 + 3.0
+puts 5.0 - 8.0
+puts 9.0 / 2.0
+
+puts 1 + 2
+puts 2 * 3
+puts 5 - 8
+puts 9 / 2
+
+puts (24 - 8) * (18 + 9) * 0.5
+
+# Hours in a year
+puts 24 * 365
+
+# Minutes in a decade (not counting leap years)
+puts (24 * 60) * (365 * 10)
+
+# Author's age in years using 1,160 million seconds -- roughly 36
+puts (1160 * 1000000) / 31536000
